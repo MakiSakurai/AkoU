db1e1449e4d8a8b4064e071df599301e118471c1
--global <--global>
2022-08-12 04:02:45 +0900

    割り込みテスト動作OK

--- src/main.cpp
+++ src/main.cpp
@@ -1,56 +1,33 @@
-// NOTE: This is a simple example that only reads the INTA or INTB pin
-//       state. No actual interrupts are used on the host microcontroller.
-//       MCP23XXX supports the following interrupt modes:
-//           * CHANGE - interrupt occurs if pin changes to opposite state
-//           * LOW - interrupt occurs while pin state is LOW
-//           * HIGH - interrupt occurs while pin state is HIGH
-
-// ok to include only the one needed
-// both included here to make things simple for example
 #include <Adafruit_MCP23X17.h>
+#include <Arduino.h>
+#define INT_PIN 14
 
-#define BUTTON_PIN 1  // MCP23XXX pin used for interrupt
-
-#define INT_PIN 14  // microcontroller pin attached to INTA/B
-
-// uncomment appropriate line
 Adafruit_MCP23X17 mcp;
-
 void setup() {
   Serial.begin(9600);
-  // while (!Serial);
-  Serial.println("MCP23xxx Interrupt Test!");
+  Serial.println("MCP23x17 Test");
 
-  // uncomment appropriate mcp.begin
   if (!mcp.begin_I2C(0x27)) {
-    // if (!mcp.begin_SPI(CS_PIN)) {
-    Serial.println("Error.");
+    Serial.println("I2C connection Error.");
     while (1)
       ;
   }
 
-  // configure MCU pin that will read INTA/B state
   pinMode(INT_PIN, INPUT);
 
-  // OPTIONAL - call this to override defaults
-  // mirror INTA/B so only one wire required
-  // active drive so INTA/B will not be floating
-  // INTA/B will be signaled with a LOW
   mcp.setupInterrupts(true, false, LOW);
 
-  // configure button pin for input with pull up
-  mcp.pinMode(BUTTON_PIN, INPUT_PULLUP);
+  mcp.pinMode(0, INPUT);
+  mcp.pinMode(8, INPUT);
 
-  // enable interrupt on button_pin
-  mcp.setupInterruptPin(BUTTON_PIN, LOW);
-
+  mcp.setupInterruptPin(0, LOW);
+  mcp.setupInterruptPin(8, LOW);
   Serial.println("Looping...");
 }
-
 void loop() {
   if (!digitalRead(INT_PIN)) {
     Serial.print("Interrupt detected on pin: ");
     Serial.println(mcp.getLastInterruptPin());
-    delay(250);  // debounce
+    delay(1000);
   }
 }

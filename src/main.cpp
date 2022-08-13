87c495d72ce8fe505fa3270968bba30e0346d359
--global <--global>
2022-08-14 02:52:13 +0900

    割り込みうごかない．．．

--- src/main.cpp
+++ src/main.cpp
@@ -2,25 +2,34 @@
 #include <Adafruit_NeoPixel.h>  //ライブラリの読み込み
 #include <Arduino.h>
 #define MAX_VAL 64  // 最大輝度の設定をする(暗い←0 ～ 255→明い)
-#define INT_PIN 14
+#define INT_PIN GPIO_NUM_14
 
 Adafruit_MCP23X17 mcp;
 // Adafruit_NeoPixel(LEDの使用個数、Arduinoのピン番号、色の並び+データの転送速度)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, 12, NEO_GRB + NEO_KHZ800);
 
+volatile byte state = LOW;
+
+void IRAM_ATTR itr_1() {
+  state = !state;
+  digitalWrite(LED_BUILTIN,state);
+  Serial.print("Interrupt detected on pin: ");
+  Serial.println(mcp.getLastInterruptPin());
+}
+
 void setup() {
-  strip.begin();  //インスタンスの使用を開始、この時全てのLEDの状態を「0」とする。
-  strip.show();  // Arduinoから全てのLEDへオフ信号「0」を転送し初期化する。
+  strip.begin();
+  strip.show(); 
   Serial.begin(9600);
   Serial.println("MCP23x17 Test");
+  pinMode(INT_PIN, INPUT);
+  pinMode(LED_BUILTIN,OUTPUT);
+  attachInterrupt(INT_PIN, itr_1, CHANGE);
 
   if (!mcp.begin_I2C(0x27)) {
     Serial.println("I2C connection Error.");
-    while (1)
-      ;
+    while (1);
   }
-
-  pinMode(INT_PIN, INPUT);
 
   mcp.setupInterrupts(true, false, LOW);
 
@@ -33,18 +42,6 @@ void setup() {
 }
 
 void loop() {
-  if (!digitalRead(INT_PIN)) {
-    Serial.print("Interrupt detected on pin: ");
-    Serial.println(mcp.getLastInterruptPin());
-    uint16_t n = strip.numPixels();  // 接続されたLEDの数を取得します
-    Serial.print("kosuu");
-    Serial.println(n);
-
-    strip.setPixelColor(3,strip.Color(255, 0,255));  // LED番号を指定して色をセットできます．一括して色を変更したい場合はfor文でLEDの数分だけ
-    // strip.setPixelColor(n, red, green, blue);
-
-    strip.setBrightness(64);  // 0-255の範囲で輝度（実際には電流量？）を調整できます
-    strip.show();
-    delay(50);
-  }
+  delay(100);
+  Serial.print(".");
 }

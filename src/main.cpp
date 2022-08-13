24dcac042a87a6be7ab705ebdf2ef57ffbfb05b7
--global <--global>
2022-08-14 03:17:12 +0900

    わりこみうごかない2

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,18 @@
 #include <Adafruit_NeoPixel.h>  //ライブラリの読み込み
 #include <Arduino.h>
 #define MAX_VAL 64  // 最大輝度の設定をする(暗い←0 ～ 255→明い)
-#define INT_PIN GPIO_NUM_14
+#define INT_PIN A6
 
 Adafruit_MCP23X17 mcp;
 // Adafruit_NeoPixel(LEDの使用個数、Arduinoのピン番号、色の並び+データの転送速度)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, 12, NEO_GRB + NEO_KHZ800);
 
 volatile byte state = LOW;
-
+volatile bool sensor[8]={false,false,false,false,false,false,false,false};
 void IRAM_ATTR itr_1() {
   state = !state;
   digitalWrite(LED_BUILTIN,state);
-  Serial.print("Interrupt detected on pin: ");
-  Serial.println(mcp.getLastInterruptPin());
+  sensor[0]=true;
 }
 
 void setup() {
@@ -24,7 +23,7 @@ void setup() {
   Serial.println("MCP23x17 Test");
   pinMode(INT_PIN, INPUT);
   pinMode(LED_BUILTIN,OUTPUT);
-  attachInterrupt(INT_PIN, itr_1, CHANGE);
+  attachInterrupt(INT_PIN, itr_1, FALLING);
 
   if (!mcp.begin_I2C(0x27)) {
     Serial.println("I2C connection Error.");
@@ -44,4 +43,11 @@ void setup() {
 void loop() {
   delay(100);
   Serial.print(".");
+  if(sensor[0]==true){
+    Serial.println("intr ch:1"); 
+    Serial.print("intr pin:");
+    Serial.println(mcp.getLastInterruptPin());
+    sensor[0]=false;
+  }
+  
 }
